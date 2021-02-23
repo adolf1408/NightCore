9a6a9d8e9bc823fd6656f4ba753682bb51d4e051
adolf1408 <79271128+adolf1408@users.noreply.github.com>
2021-02-23 15:07:56 +0800

    Update gban.py

--- userbot/modules/gban.py
+++ userbot/modules/gban.py
@@ -68,7 +68,7 @@ async def global_ban(event):
             "`You need to be at least admin in 1 group to gban someone!`"
         )
     await event.edit(
-        f"Global Banned! [{user.first_name}](tg://user?id={user.id}) Jumlah Grup `{len(groups_admin)}` Grup"
+        f"GBANNED JAMET [{user.first_name}](tg://user?id={user.id}) DI `{len(groups_admin)}` GRUP"
     )
     for i in range(len(groups_admin)):
         try:
